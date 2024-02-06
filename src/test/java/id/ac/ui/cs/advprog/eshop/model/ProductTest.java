427073914fad1ce20c0c763113d87a5b95971366
novrizair <novrizal.airsyahputra@ui.ac.id>
2024-02-06 23:36:42 +0700

    exercise 2: functional test

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -13,6 +13,7 @@ class ProductTest {
         this.product.setProductName("Sampo Cap Bambang");
         this.product.setProductQuantity(100);
     }
+
     @Test
     void testGetProductId() {
         assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product.getProductId());
