e4a763236c8afd31b4b3f4d1d4495526de9a68dc
novrizair <novrizal.airsyahputra@ui.ac.id>
2024-02-13 18:37:43 +0700

    fix unnecessary modifiers at ProductService.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,10 +4,10 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
+    Product create(Product product);
+    List<Product> findAll();
     Product findById(String id);
 
-    public Product edit(Product product);
-    public Product delete(Product product);
+    Product edit(Product product);
+    Product delete(Product product);
 }
