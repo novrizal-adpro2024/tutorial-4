ee0b68dffd8bbf5fcacbc5a9176794db58653e89
novrizair <novrizal.airsyahputra@ui.ac.id>
2024-03-05 19:03:47 +0700

    [RED] Add Payment model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
